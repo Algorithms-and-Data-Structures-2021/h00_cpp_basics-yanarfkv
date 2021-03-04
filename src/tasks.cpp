c507d8564e91cf6fe12cda7617f4ad9e4ecbd61c
yanarfkv <yana.rafikova2405@mail.ru>
2021-03-04 09:55:08 +0300

    New commit

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -39,14 +39,12 @@ int **allocate_2d_array(int num_rows, int num_cols, int init_value) {
 bool copy_2d_array(int **arr_2d_source, int **arr_2d_target, int num_rows, int num_cols) {
     if ((arr_2d_source == nullptr || arr_2d_target == nullptr) && (num_cols < 1 || num_rows < 1))
         return false;
-    else {
-        for (int i = 0; i < num_rows; i++) {
-            for (int j = 0; j < num_cols; j++) {
-                arr_2d_target[i][j] = arr_2d_source[i][j];
-            }
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            arr_2d_target[i][j] = arr_2d_source[i][j];
         }
-        return true;
     }
+    return true;
 }
 
 // Задание 4
@@ -76,17 +74,14 @@ void reverse_1d_array(int *arr_begin, int *arr_end) {
 
 // Задание 6
 int *find_max_element(int *arr, int size) {
-    int max = 0;
-    int *pointer_max = &max;
-    if (arr == nullptr) return nullptr;;
-    if (size > 0) {
-        for (int i = 0; i < size; i++) {
-            if (arr[i] > *pointer_max) {
-                *pointer_max = arr[i];
-            }
+    int *max = arr;
+    if (arr == nullptr || size < 1) return nullptr;;
+    for (int i = 0; i < size; i++) {
+        if (*arr > *max) {
+            max = arr;
         }
-    } else return nullptr;
-    return pointer_max;
+    }
+    return max;
 }
 
 // Задание 7
